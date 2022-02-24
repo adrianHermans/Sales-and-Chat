d315d62994c30dded67e370f4c0b97746c53f850
ADRIAN DOMINIQUE HERMANS <adrianhermans@gmail.com>
2022-02-24 15:50:02 -0300

    Language Dropdown

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// document.querySelector(".right ul li").addEventListener("click", function(){
-//     this.toggleClass("active");
-//   });
-$(document).ready(function() {
-    console.log('aaaaaaaaaaaaaaa');
-})
-
-$('.right ul li').click( element => {
-    console.log('clicou');
-    $('.dropdown').toggleClass('dNone')
-    console.log('.dropdown');
-})
+   document.querySelector(".nav-right ul li").addEventListener("click", function(){
+    this.classList.toggle("active");
+  });
+  
