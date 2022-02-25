c8f95fcca66b0d31365fe049afe88cbe71dbd62d
ADRIAN DOMINIQUE HERMANS <adrianhermans@gmail.com>
2022-02-25 16:57:43 -0300

    Finishing

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
-  document.querySelectorAll(".drop").forEach(el=>{
+ document.querySelectorAll(".drop").forEach(el=>{
     el.addEventListener("click", function(){
         this.classList.toggle("active");
       });
   })
+  // $('.languages').find('option:first').attr('selected', 'selected');
+  // $('.languages').val(0);
+  // $('.languages option:eq(0)').attr('selected','selected');
+  // $('.languages').get(0).selectedIndex = 0;
